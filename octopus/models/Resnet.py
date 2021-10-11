92619e236ed9ddda27be2d698d3c779cec429460
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-10-11 12:22:10 -0400

    Added resnet18 to code

--- octopus/models/Resnet.py
+++ octopus/models/Resnet.py
@@ -8,6 +8,7 @@ import logging
 
 import torch.nn as nn
 
+
 def _calc_output_size(input_size, padding, dilation, kernel_size, stride):
     input_size_padded = input_size + 2 * padding
     kernel_dilated = (kernel_size - 1) * (dilation - 1) + kernel_size
@@ -37,65 +38,50 @@ class SimpleResidualBlock(nn.Module):
         return out
 
 
+# https://towardsdatascience.com/residual-network-implementing-resnet-a7da63c7b278
 class ResidualBlock(nn.Module):
 
-    def __init__(self, in_channel, out_channel, stride=1):
+    def __init__(self, in_channels, out_channels):
         super().__init__()
 
-        # first conv layer
-        self.conv1 = nn.Conv2d(in_channel, out_channel, kernel_size=3, stride=stride, padding=1, bias=False)
-        self.bn1 = nn.BatchNorm2d(out_channel)
+        self.expansion = 1
+        self.in_channels = in_channels
+        self.out_channels = out_channels
+        self.stride = 2 if in_channels != out_channels else 1
 
-        # second conv layer
-        self.conv2 = nn.Conv2d(out_channel, out_channel, kernel_size=3, stride=1, padding=1, bias=False)
-        self.bn2 = nn.BatchNorm2d(out_channel)
+        self.blocks = nn.Sequential(
+
+            # first conv layer
+            nn.Conv2d(self.in_channels, self.out_channels, kernel_size=3, stride=self.stride, padding=1, bias=False),
+            nn.BatchNorm2d(self.out_channels),
+            nn.ReLU(inplace=True),
+
+            # second conv layer
+            nn.Conv2d(self.out_channels, self.out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(self.out_channels))
 
         # shortcut
-        if stride == 1:
+        if self.in_channels == self.out_channels:
             self.shortcut = nn.Identity()
         else:
-            self.shortcut = nn.Conv2d(in_channel, out_channel, kernel_size=1, stride=stride)
-        self.relu = nn.ReLU()
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(self.in_channels, self.out_channels, kernel_size=1, stride=self.stride, bias=False),
+                nn.BatchNorm2d(self.out_channels)
+            )
+
+        self.activate = nn.ReLU(inplace=True)
 
     def forward(self, x):
-        out = self.conv1(x)
-        out = self.bn1(out)
+        # blocks
+        out = self.blocks(x)
 
-        out = self.conv2(out)
-        out = self.bn2(out)
-
+        # shortcut
         shortcut = self.shortcut(x)
 
-        out = self.relu(out + shortcut)
+        # combine
+        out = self.activate(out + shortcut)
 
         return out
-
-
-class ClassificationNetwork(nn.Module):
-    def __init__(self, in_features, num_classes):
-        super().__init__()
-
-        self.layers = nn.Sequential(
-            nn.Conv2d(in_features, 64, kernel_size=7, stride=2, padding=1, bias=False),
-            # nn.BatchNorm2d(64),  # ?? include or not
-            # nn.ReLU(),
-            nn.MaxPool2d(kernel_size=3, stride=2),
-            SimpleResidualBlock(64),
-            SimpleResidualBlock(64),
-            SimpleResidualBlock(64),
-            SimpleResidualBlock(64),
-            nn.AdaptiveAvgPool2d((1, 1)),
-            nn.Flatten(),
-        )
-        self.linear = nn.Linear(64, num_classes)
-
-    def forward(self, x, return_embedding=False):
-        embedding = self.layers(x)
-
-        if return_embedding:
-            return embedding
-        else:
-            return self.linear(embedding)
 
 
 class Resnet18(nn.Module):
@@ -103,28 +89,29 @@ class Resnet18(nn.Module):
         super().__init__()
 
         self.layers = nn.Sequential(
-            nn.Conv2d(in_features, out_channels=64, kernel_size=7, stride=2, padding=1, bias=False),
+            nn.Conv2d(in_channels=3, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
             nn.BatchNorm2d(64),
             nn.ReLU(),
 
             nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
 
-            ResidualBlock(64, 64, stride=2),
-            ResidualBlock(64, 64, stride=1),
+            # layer 1
+            ResidualBlock(64, 64),
+            ResidualBlock(64, 64),
 
-            ResidualBlock(64, 128, stride=2),
-            ResidualBlock(128, 128, stride=1),
+            ResidualBlock(64, 128),
+            ResidualBlock(128, 128),
 
-            ResidualBlock(128, 256, stride=2),
-            ResidualBlock(256, 256, stride=1),
+            ResidualBlock(128, 256),
+            ResidualBlock(256, 256),
 
-            ResidualBlock(256, 512, stride=2),
-            ResidualBlock(512, 512, stride=1),
+            ResidualBlock(256, 512),
+            ResidualBlock(512, 512),
 
             nn.AdaptiveAvgPool2d((1, 1)),
             nn.Flatten(),
         )
-        self.linear = nn.Linear(64, num_classes)
+        self.linear = nn.Linear(512, num_classes)
 
     def forward(self, x, return_embedding=False):
         embedding = self.layers(x)
