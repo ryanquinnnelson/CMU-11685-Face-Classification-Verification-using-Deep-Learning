dc0e929a1b99ab79151fe986f04a12a853da10cb
ryanquinnnelson <ryan.q.nelson@gmail.com>
2021-10-12 12:31:54 -0400

    working on resnet50

--- octopus/models/resnets.py
+++ octopus/models/resnets.py
@@ -119,9 +119,11 @@ class Resnet18(nn.Module):
         else:
             return self.linear(embedding)
 
+
 class Resnet34(nn.Module):
-    def __init__(self, in_features, num_classes):
+    def __init__(self, in_features, num_classes, init_type=None):
         super().__init__()
+        self.init_type = init_type
 
         self.layers = nn.Sequential(
             # conv1
@@ -163,6 +165,100 @@ class Resnet34(nn.Module):
         self.linear = nn.Sequential(
             nn.Linear(512, num_classes))
         # nn.Softmax(dim=1))  # removed because it stopped model from improving and I don't know why. Could be dim=1.
+
+    #     # apply initialization if defined
+    #     self.layers.apply(self._initialize_weights)
+    #
+    # def _initialize_weights(self, seq):
+    #     logging.info(f'Initializing weights using {self.init_type}...')
+
+    def forward(self, x, return_embedding=False):
+        embedding = self.layers(x)
+
+        if return_embedding:
+            return embedding
+        else:
+            return self.linear(embedding)
+
+
+class BottleneckResidualBlock(nn.Module):
+    def __init__(self, in_channels, out_channels):
+        super().__init__()
+
+        self.stride = 2 if in_channels != out_channels else 1
+
+        self.blocks = nn.Sequential(
+
+            # first conv layer
+            nn.Conv2d(in_channels, out_channels, kernel_size=1, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            # second conv layer
+            nn.Conv2d(out_channels, out_channels, kernel_size=3, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels),
+            nn.ReLU(inplace=True),
+
+            # third conv layer
+            nn.Conv2d(out_channels, out_channels * 4, kernel_size=1, stride=1, padding=1, bias=False),
+            nn.BatchNorm2d(out_channels * 4),
+        )
+
+        # shortcut
+        if in_channels == out_channels:
+            self.shortcut = nn.Identity()
+        else:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_channels, out_channels * 4, kernel_size=1, stride=1, bias=False),
+                nn.BatchNorm2d(out_channels * 4)
+            )
+        self.activate = nn.ReLU(inplace=True)
+
+
+class Resnet50(nn.Module):
+    def __init__(self, in_features, num_classes, init_type=None):
+        super().__init__()
+        self.init_type = init_type
+
+        self.layers = nn.Sequential(
+            # conv1
+            nn.Conv2d(in_channels=in_features, out_channels=64, kernel_size=7, stride=2, padding=3, bias=False),
+            nn.BatchNorm2d(64),
+            nn.ReLU(),
+
+            nn.MaxPool2d(kernel_size=3, stride=2, padding=1),
+
+            # conv2..x
+            BottleneckResidualBlock(64, 64),
+            BottleneckResidualBlock(64, 64),
+            BottleneckResidualBlock(64, 64),
+
+            # conv3..x
+            BottleneckResidualBlock(64, 128),
+            BottleneckResidualBlock(128, 128),
+            BottleneckResidualBlock(128, 128),
+            BottleneckResidualBlock(128, 128),
+
+            # conv4..x
+            BottleneckResidualBlock(128, 256),
+            BottleneckResidualBlock(256, 256),
+            BottleneckResidualBlock(256, 256),
+            BottleneckResidualBlock(256, 256),
+            BottleneckResidualBlock(256, 256),
+            BottleneckResidualBlock(256, 256),
+
+            # conv5..x
+            BottleneckResidualBlock(256, 512),
+            BottleneckResidualBlock(512, 512),
+            BottleneckResidualBlock(512, 512),
+
+            # summary
+            nn.AdaptiveAvgPool2d((1, 1)),
+            nn.Flatten(),
+        )
+        # decoding layer
+        self.linear = nn.Sequential(
+            nn.Linear(512, num_classes))
 
     def forward(self, x, return_embedding=False):
         embedding = self.layers(x)
